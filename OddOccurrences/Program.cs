28dd379faf0beb872bc3387038b4dbecd79ce17e
ddunchev <danchodunchev@gmail.com>
2017-05-06 10:08:28 +0300

    Commit to Dictionaries

--- OddOccurrences/Program.cs
+++ OddOccurrences/Program.cs
@@ -10,6 +10,16 @@ namespace OddOccurrences
     {
         static void Main(string[] args)
         {
+            var myDict = new Dictionary<string, int>();
+
+            myDict.Add("Dunchev", 42);
+            myDict.Add("Stanchevska", 38);
+            myDict.Add("Ivanova", 9);
+
+            foreach (KeyValuePair<string, int> item in myDict)
+            {
+                Console.WriteLine($"Name: {item.Key}, Age: {item.Value}");
+            }
         }
     }
 }
