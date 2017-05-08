77dc3b9a09d60b61b209108b520c245c1eece165
ddunchev <danchodunchev@gmail.com>
2017-05-08 04:37:22 +0300

    Upload a bit more examples

---
+++ Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>()
+            {
+                {"apple", 1},
+                {"appricot", 2},
+                {"banana", 5}
+            };
+
+
+            var list = new List<string>(dict.Keys);
+
+            var s1 = Stopwatch.StartNew();
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item} -> {dict[item]}");
+            }
+
+            s1.Stop();
+
+            Console.WriteLine(s1.Elapsed.TotalMilliseconds);
+        }
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = new string[]{
+                "banana",
+                "appricot",
+                "peach"
+            };
+
+            var myDict = myString.ToDictionary(item => item, item => 0);
+
+            foreach (var item in myDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+
+        }
+    }
+}
