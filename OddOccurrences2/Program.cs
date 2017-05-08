2425d2fb328200d2abf2b3031ea1c9b8120784df
ddunchev <danchodunchev@gmail.com>
2017-05-08 03:08:37 +0300

    Commit Second variant

---
+++ OddOccurrences2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrences2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"E:\input.txt"));
+#endif
+
+            var input = Console.ReadLine().ToLower().Split();
+
+            var myDict = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if (myDict.ContainsKey(word)) myDict[word]++;
+                else myDict[word] = 1;
+            }
+
+            var myList = new List<string>();
+
+            foreach (KeyValuePair<string, int> pair in myDict)
+            {
+                if (pair.Value % 2 != 0) myList.Add(pair.Key);
+            }
+
+            Console.WriteLine(String.Join(", ", myList));
+        }
+    }
+}
